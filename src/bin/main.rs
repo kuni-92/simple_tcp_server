b704944a10bb4b59e28b8db87c9427dbf617d530
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-12-18 20:40:15 +0900

    Read and display request.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,10 @@
-use std::net::TcpListener;
+use std::{io::Read, error::Error, net::{TcpListener, TcpStream}};
 
 fn main() {
     match TcpListener::bind("127.0.0.1:60080") {
         Ok(server) => {
             println!("Starting TCP server.");
-            handle_server(server);
+       handle_server(server);
         },
         Err(e) => {
             panic!("Tcplistener binding error. Error detail:{}", e);
@@ -12,8 +12,20 @@ fn main() {
     };
 }
 
-fn handle_server(s: TcpListener) {
-    for stream in s.incoming().take(2) {
+fn handle_server(listener: TcpListener) {
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
         println!("Getting request!");
+        let request = read_request(stream).unwrap();
+        let request = std::str::from_utf8(&request).unwrap();
+        println!("{}", request);
+    }
+}
+
+fn read_request(mut stream: TcpStream) -> Result<Box<[u8]>, Box<dyn Error>> {
+    let mut buffer = [0; 1024];
+    match stream.read(&mut buffer) {
+        Ok(_) => Ok(Box::new(buffer)),
+        Err(e) => Err(Box::new(e)),
     }
 }
