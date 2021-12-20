1f679a0536e7775698021bb2a1546cc906cc57b5
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-12-20 21:38:05 +0900

    Modularize request and response functions.

--- src/listener.rs
+++ src/listener.rs
@@ -1,9 +1,31 @@
-use std::net::{ToSocketAddrs, TcpListener, TcpStream};
-use std::io::{Read, Write};
-use std::error::Error;
+use std::net::{ToSocketAddrs, TcpListener};
 
 pub struct SimpleListener {
     listener: TcpListener,
+}
+
+mod  request {
+    use std::net::TcpStream;
+    use std::io::Read;
+    use std::error::Error;
+
+    pub fn read(mut stream: &TcpStream) -> Result<Box<[u8]>, Box<dyn Error>> {
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(_) => Ok(Box::new(buffer)),
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+}
+
+mod response {
+    use std::net::TcpStream;
+    use std::io::Write;
+
+    pub fn write(mut stream: &TcpStream, response: String) {
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 impl SimpleListener {
@@ -25,21 +47,13 @@ impl SimpleListener {
         for stream in self.listener.incoming().take(2) {
             println!("Getting request!");
 
-            let mut stream = stream.unwrap();
-            let request = read_request(&stream).unwrap();
+            let stream = stream.unwrap();
+            let request = request::read(&stream).unwrap();
             let request = std::str::from_utf8(&request).unwrap();
             println!("{}", request);
 
-            let response = String::from("HTTP/1.1 200 OK \r\n\r\n");
-            stream.write(response.as_bytes()).unwrap();
+            let response_content = String::from("HTTP/1.1 200 OK \r\n\r\n");
+            response::write(&stream, response_content);
         }
-    }
-}
-
-fn read_request(mut stream: &TcpStream) -> Result<Box<[u8]>, Box<dyn Error>> {
-    let mut buffer = [0; 1024];
-    match stream.read(&mut buffer) {
-        Ok(_) => Ok(Box::new(buffer)),
-        Err(e) => Err(Box::new(e)),
     }
 }
